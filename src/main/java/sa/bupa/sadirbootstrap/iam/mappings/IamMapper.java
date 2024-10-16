4c5a53b5201a35f78b007b2c44c995c534b1d985
devineosoft <devi.dinesh@neosoftmail.com>
2024-10-16 14:57:22 +0530

    IamMapper logic additions

--- src/main/java/sa/bupa/sadirbootstrap/iam/mappings/IamMapper.java
+++ src/main/java/sa/bupa/sadirbootstrap/iam/mappings/IamMapper.java
@@ -3,6 +3,7 @@ package sa.bupa.sadirbootstrap.iam.mappings;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
 import sa.bupa.sadirbootstrap.iam.data.SdrAuthoritiesModel;
 import sa.bupa.sadirbootstrap.iam.data.enumrations.SdrModulePermissions;
 import sa.bupa.sadirbootstrap.iam.domain.SdrAuthority;
@@ -12,16 +13,32 @@ import java.util.Map;
 @Mapper
 public interface IamMapper {
 
+    IamMapper INSTANCE = Mappers.getMapper(IamMapper.class);
+
     Map<String,SdrModulePermissions> patternToPermissionMap = Map.of("c", SdrModulePermissions.CREATE,
             "r", SdrModulePermissions.READ,
             "u", SdrModulePermissions.UPDATE,
             "d", SdrModulePermissions.DELETE);
+
+    Map<SdrModulePermissions,String> permissionToCharMap = Map.of(SdrModulePermissions.CREATE,"c",
+            SdrModulePermissions.READ,"r",
+            SdrModulePermissions.UPDATE,"u",
+            SdrModulePermissions.DELETE,"d");
 
 
     @Mappings({
             @Mapping(target = "accessPattern", expression = "java(mapAuthorityPermissionsToPattern(authoritiesModel.create, authoritiesModel.update, authoritiesModel.read, authoritiesModel.delete))")
     })
     SdrAuthority authorityModelToAuthority(SdrAuthoritiesModel authoritiesModel);
+
+
+    @Mappings({
+            @Mapping(target = "create", expression = "java(patternToDeleteSdrModulePermission(sdrAuthority.accessPattern, SdrModulePermissions.CREATE))"),
+            @Mapping(target = "read", expression = "java(patternToDeleteSdrModulePermission(sdrAuthority.accessPattern, SdrModulePermissions.READ))"),
+            @Mapping(target = "update", expression = "java(patternToDeleteSdrModulePermission(sdrAuthority.accessPattern, SdrModulePermissions.UPDATE))"),
+            @Mapping(target = "delete", expression = "java(patternToDeleteSdrModulePermission(sdrAuthority.accessPattern, SdrModulePermissions.DELETE))")
+    })
+    SdrAuthoritiesModel authorityToAuthorityModel(SdrAuthority sdrAuthority);
 
 
     default String mapAuthorityPermissionsToPattern(SdrModulePermissions create, SdrModulePermissions update, SdrModulePermissions read, SdrModulePermissions delete){
@@ -66,5 +83,9 @@ public interface IamMapper {
         return null;
     }
 
-
+    default SdrModulePermissions patternToDeleteSdrModulePermission(String pattern, SdrModulePermissions sdrModulePermission) {
+        if (null != pattern && pattern.contains(permissionToCharMap.get(sdrModulePermission)))
+            return sdrModulePermission;
+        return null;
+    }
 }
