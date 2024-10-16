fabd1f4f51fd860776089a51180b90c28406a3cc
shibinDnl <shibinDnl@gmail.com>
2024-10-16 18:44:22 +0530

    Mapping logic for authorized person and customer

--- src/main/java/sa/bupa/sadirbootstrap/iam/domain/identities/impl/AuthorizedPerson.java
+++ src/main/java/sa/bupa/sadirbootstrap/iam/domain/identities/impl/AuthorizedPerson.java
@@ -1,7 +1,15 @@
 package sa.bupa.sadirbootstrap.iam.domain.identities.impl;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import sa.bupa.sadirbootstrap.iam.domain.SdrIdentity;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorizedPerson extends SdrIdentity {
     private String authorizedId;
 }
